6a7cefa5e34f0242748432ce6d084d7235065fb2
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-01-12 14:28:37 +0000

    backend added

---
+++ notes-backend-server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+let notes = [
+  'This note is coming from the server'
+];
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/notes', (_req, res) => {
+  res.send(JSON.stringify(notes));
+});
+
+app.post('/notes', (req, res) => {
+  notes.push(req.body.content)
+  res.send(JSON.stringify(notes));
+});
+
+app.delete('/notes', (req, res) => {
+  notes = [];
+  res.send(JSON.stringify(notes))
+});
+
+app.listen(PORT);
+
