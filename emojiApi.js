80d2bd4dd78c9af361c112c137c69ff9451aa5ac
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-01-14 15:24:58 +0000

    refactoring

--- emojiApi.js
+++ emojiApi.js
@@ -1,15 +1,5 @@
 const axios = require('axios')
 
-// const newNote = {"text": "Hello, :earth_africa:"}
-
-// fetch('https://makers-emojify.herokuapp.com/', {
-//       method: 'POST',
-//       headers: {
-//         'Content-type': 'application/json',
-//       },
-//       body: JSON.stringify(newNote),
-//     })
-
 class EmojiApi {
 
   async convertToEmoji(text, callback) {
@@ -18,20 +8,8 @@ class EmojiApi {
     })
     .then(response => callback(response.data.emojified_text))
     .catch(e => console.log(e));
+    
   }
 }
 
 module.exports = EmojiApi;
-
-
-
-  // convertToEmoji(text) {
-  //   const newNote = {"text": text}
-  //   fetch('https://makers-emojify.herokuapp.com/', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-type': 'application/json',
-  //     },
-  //     body: JSON.stringify(newNote),
-  //   }).then((response) => response.json()).then((data) => { return data.emojified_text });
-  // }
