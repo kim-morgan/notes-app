9f8320d32047f9b67c3d1ebb0b25d5abaa262e90
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-01-14 15:21:48 +0000

    deleting commented out tests

--- emojiApi.test.js
+++ emojiApi.test.js
@@ -1,28 +1,9 @@
 const EmojiApi = require("./emojiApi");
-// const NotesApi = require("./NotesApi");
-// const EmojiApi = require("./EmojiApi");
-const axios = require('axios')
-// jest.mock("axios");
-
-// require("jest-fetch-mock").enableMocks();
+const axios = require('axios');
 
 describe("Emoji", () => {
   it("#convertToEmoji - converts text with emojis", async () => {
-    // const api = new EmojiApi();
-    // const response = [
-    //   {
-    //     status: "OK",
-    //     text: "Hello, :earth_africa:",
-    //     emojified_text: "Hello, 🌍",
-    //   },
-    // ];
-    // axios.post.mockResolvedValueOnce(response)
-
     const api = new EmojiApi();
-    // const emojiApi = new EmojiApi();
-    // const model = new NotesModel();
-    // const view = new NotesView(model, api, emojiApi);
-
     const emojifiedText = await api.convertToEmoji("Hello, :earth_africa:", (res) => res);
     expect(emojifiedText).toEqual("Hello, 🌍");
   });
